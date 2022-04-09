0c3fe295cb2a40ff30d3a8e57645bb276187736c
Sujeewa Sandeepa <sujeewasandeepa@protonmail.ch>
2022-04-09 16:50:45 +0530

    create basic ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="container">
+        <div class="row mt-5">
+          <div class="col">
+            <h3>Greetings!</h3>
+            <p>Contract Balance: 0</p>
+          </div>
+          <div class="col">
+            <form>
+              <div class="mb-3">
+                <input type="number" class="form-control" placeholder="0" value="test" />
+              </div>
+              <button type="submit" class="btn btn-success">Deposit</button>
+            </form>
+            <form class="mt-5">
+              <div class="mb-3">
+                <input type="text" class="form-control" value="test" />
+              </div>
+              <button type="submit" class="btn btn-dark">Deposit</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
